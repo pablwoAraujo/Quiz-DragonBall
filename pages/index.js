1d0cc70464ce0a5bbd926a6139b4fc734b82342a
pablwoAraujo <pablwo.araujo@ccc.ufcg.edu.br>
2021-01-28 23:50:42 -0300

    Next Router and some quiz questions were added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
-import QuizLogo from '../src/components/QuizLogo'
+import QuizLogo from '../src/components/QuizLogo';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
 
-export const QuizContainer = styled.div`
+const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -18,8 +24,14 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.title}</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -27,7 +39,21 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                placeholder="Diz ae seu nome pra jogar!"
+                onChange={(info) => setName(info.target.value)}
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
 
